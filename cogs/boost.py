cecec87cae54760ebf4c5e1d1048af7e9f8be660
Daishiky <pistolamario0@gmail.com>
2024-01-08 15:47:14 -0600

    utc time in embed footers

--- cogs/boost.py
+++ cogs/boost.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import time
+from datetime import datetime,timezone
 
 class Boost(commands.Cog):
 
@@ -11,6 +11,7 @@ class Boost(commands.Cog):
     async def on_message(self, message):
         
         if "MessageType.premium_guild" in str(message.type):
+            tm = datetime.now(timezone.utc)
             
             channel = self.bot.get_channel(1192457359239417916)
             db = discord.utils.get(message.guild.roles, id=1192951322350190783)
@@ -21,14 +22,14 @@ class Boost(commands.Cog):
                 color = 0xffc0cb
             )
             server_boost_embed.set_author(name=f"⇾ 💃 SLURP CREW Annuncio Boost ⇽", icon_url=message.guild.icon.url)
-            server_boost_embed.set_footer(text=f"Oggi alle {time.strftime('%H:%M')}", icon_url=message.guild.icon.url)
+            server_boost_embed.set_footer(text=f"Oggi alle {tm.strftime('%H:%M')}", icon_url=message.guild.icon.url)
             
             double_boost_embed = discord.Embed(
                 description=f"# 🦋 NUOVO BOOST 🦋 #\n\n<:discordboost:1192556344159522986> Grazie mille per il secondo boost {message.author.mention} <3\n<:3503rolemanager:1192554243861794817> Hai ricevuto il ruolo {db.mention} ~!",
                 color = 0xffc0cb
             )
             double_boost_embed.set_author(name=f"⇾ 💃 SLURP CREW Annuncio Double Boost ⇽", icon_url=message.guild.icon.url)
-            double_boost_embed.set_footer(text=f"Oggi alle {time.strftime('%H:%M')}", icon_url=message.guild.icon.url)
+            double_boost_embed.set_footer(text=f"Oggi alle {tm.strftime('%H:%M')}", icon_url=message.guild.icon.url)
             
             if "Server Booster" in message.author.roles:
                 await message.author.add_roles(db)
--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import time
+from datetime import datetime,timezone
 
 class Welcome(commands.Cog):
 
@@ -10,23 +10,26 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         if member.guild.id == 1192455862464299058:
+            
+            tm = datetime.now(timezone.utc)
+            
             embed_w = discord.Embed(description=f"# ✨ NUOVO MEMBRO ✨ # \n👋 Hey {member.mention} ~!\n\n🪐 Benvenuto nella 💃 **SLURP Crew** <3\n\n💫 Siamo un piccolo server Discord creato da alcuni amici, miriamo a creare un luogo sicuro in cui tutti possano divertirsi, così come"
                                         " avere conversazioni interessanti. :)\n\n"
                                         "# 🪐 Links Utili 🪐 #\n"
                                         "- 🖇️ [Link d'invito](https://discord.com/invite/wsBDG3ZXUf)\n"
                                         "- 👁️‍🗨️ [Pagina DISBOARD (Recensioni)](https://disboard.org/it/server/1192455862464299058)\n"
                                         "- 💭 [Chat Generale](https://discord.com/channels/1192455862464299058/1192457359239417916)\n"
-                                        "- <a:slurp_icon:1190789464797216899> [Ruolo Verificato](https://discord.com/channels/1192455862464299058/1192457347352772699)\n"
-                                        "- <:vslticket:1190657441956896830> [Tickets](https://discord.com/channels/1192455862464299058/1192457412121215097)\n\n"
+                                        "- <a:1963blackbutterfly:1193494553160843316> [Ruolo Verificato](https://discord.com/channels/1192455862464299058/1192457347352772699)\n"
+                                        "- <:vslticket:1192554270831169536> [Tickets](https://discord.com/channels/1192455862464299058/1192457412121215097)\n\n"
                                         "🎊 Siamo sempre attivi! Dai un'occhio al canale [**🎉 Giveaways**](https://discord.com/channels/1192455862464299058/1192457351593214095) per vincere premi fantastici!", color=0xffc0cb)
             embed_w.set_author(name=f"⇾ Ciao {member.name} ⇽", icon_url=member.avatar.url if member.avatar.url != None else member.guild.icon.url)
             embed_w.set_image(url="https://cdn.discordapp.com/attachments/1192457322337947748/1192488696398749746/server_banner.gif?ex=65a942a4&is=6596cda4&hm=2a01caf8596219071e01cbd25356494f93e11958594c58b82644d471ef3be6c3&")
-            embed_w.set_footer(text=f"Oggi alle {time.strftime('%H:%M')}", icon_url=member.guild.icon.url)
+            embed_w.set_footer(text=f"Oggi alle {tm.strftime('%H:%M')}", icon_url=member.guild.icon.url)
             
             embed_g = discord.Embed(description=f"👋 Benvenuto in **{member.guild.name}**, manda il tuo primo messaggio qui!", color=0xffc0cb)
             embed_g.set_author(name=f"⇾ {member.name} ~! ⇽", icon_url=member.avatar.url if member.avatar.url != None else member.guild.icon.url)
             embed_g.set_thumbnail(url=member.avatar.url if member.avatar.url != None else member.guild.icon.url)
-            embed_g.set_footer(text=f"Oggi alle {time.strftime('%H:%M')}", icon_url=member.guild.icon.url)
+            embed_g.set_footer(text=f"Oggi alle {tm.strftime('%H:%M')}", icon_url=member.guild.icon.url)
             
             channel_welcome = self.bot.get_channel(1192457329250156654)
             channel_general = self.bot.get_channel(1192457359239417916)
